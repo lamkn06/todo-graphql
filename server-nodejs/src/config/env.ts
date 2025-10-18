55f1750ff5dfb39b61aec715d9fcbb63247db781
lam.khuong <lamkn06@gmail.com>
2025-10-18 10:42:37 +0700

    feat: init DB

--- server-nodejs/src/config/env.ts
+++ server-nodejs/src/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config({ path: '.env' });
 
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV ?? 'development',
-  PORT: process.env.PORT ? Number(process.env.PORT) : 9000,
+  PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
   DATABASE_URL: process.env.DATABASE_URL ?? '',
   JWT_SECRET: process.env.JWT_SECRET ?? '',
 };
